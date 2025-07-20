89e06ccb2b0eb93a5ef635f90e1e6ea94727521f
Imran Ahmad <imran@tm.moneyowl.com.sg>
2025-07-20 14:48:39 +0530

    DSL1 added

---
+++ dsl1.groovy
@@ -0,0 +1,34 @@
+pipelineJob('Demo-Pipeline-Job') {
+    description('Created via DSL seed script.')
+
+    definition {
+        cps {
+            script("""
+                node {
+                    stage('Set Build Env') {
+                        echo 'Setting build environment variables and configurations.'
+                    }
+                    stage('Clone and Copy Properties') {
+                        echo 'Cloning repositories and copying property files.'
+                    }
+                    stage('Maven Package') {
+                        echo 'Packaging with Maven.'
+                    }
+                    stage('Reporting in Jenkins') {
+                        echo 'Reporting test coverage in Jenkins.'
+                    }
+                    stage('Docker Build') {
+                        echo 'Building Docker image.'
+                    }
+                    stage('Docker Push') {
+                        echo 'Pushing Docker image to ECR.'
+                    }
+                    stage('Helm Deploy') {
+                        echo 'Deploying to Kubernetes via Helm.'
+                    }
+                }
+            """.stripIndent())
+        }
+    }
+}
+
